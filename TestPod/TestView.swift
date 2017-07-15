07b9b4027fb36b84bd1e9af0c9082e52a3e3ac5a
mardiyajayesh <mardiyajayesh@gmail.com>
2017-07-15 10:25:09 +0530

    init

---
+++ TestPod/TestView.swift
@@ -0,0 +1,37 @@
+//
+//  TestView.swift
+//  TestPod
+//
+//  Created by Jayesh on 15/07/17.
+//  Copyright © 2017 Jayesh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TestView: UIView {
+    
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    var colorCounter = 0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // The Main Stuff
+        
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in  //1
+            UIView.animate(withDuration: 2.0) {  //2
+                self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor  //3
+                self.colorCounter+=1  //4
+            }
+        }
+        
+        scheduledColorChanged.fire()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // You don't need to implement this
+    }
+}
